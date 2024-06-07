d83a09cedb8a29731da867a60570da69264c6ff1
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2024-06-07 11:43:11 +0530

    ...

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <ul>
+        <li>
+          <p>Teerthankar.ai</p>
+        </li>
+        <li>
+          <a href="https://github.com/RishabhJain-11">Github</a>
+        </li>
+      </ul>
+    </>
+  );
+}
